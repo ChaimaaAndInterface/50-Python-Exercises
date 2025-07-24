321f5bb10046d231d00fcc7a5c56cdcb9500d53f
Mehedi <mehedi.interface@gmail.com>
2025-07-24 19:09:47 +0300

    added reverse string

---
+++ 5_String_Reversal.py
@@ -0,0 +1,12 @@
+"""
+String Reversal: Write a function that reverses a given string.
+"""
+
+
+def reverse_string(s):
+    return s[::-1]
+
+
+my_string = "Hello"
+reverse_my_string = reverse_string(my_string)
+print(reverse_my_string)
